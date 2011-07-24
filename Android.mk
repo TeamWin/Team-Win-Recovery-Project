4b8b61751904aaf25e75262b8fc08fc900c10155
toastcfh <toastcfh@gmail.com>
2011-07-24 10:59:17 -0400

    Android.mk: fix issue with API version. fake version 2 tho we are version 3.

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@ LOCAL_MODULE := recovery
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
-RECOVERY_API_VERSION := 3
+RECOVERY_API_VERSION := 2
 LOCAL_CFLAGS += -DRECOVERY_API_VERSION=$(RECOVERY_API_VERSION)
 
 # This binary is in the recovery ramdisk, which is otherwise a copy of root.
